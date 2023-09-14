47f82f373cd42bd182798888d8b03f558004673e
michael-mb <mbonimichael@yahoo.fr>
2023-09-14 06:17:49 +0200

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header header-one">
+    <div class="header-left header-left-one">
+      <router-link to="/" class="remove-link-attr"><p style="font-size: 22px;" class="wom-title"><i class="feather-repeat fs-30"></i> GReQL - Converter</p></router-link>
+    </div>
+    <a href="javascript:void(0);" id="toggle_btn">
+      <i class="fas fa-bars"></i>
+    </a>
+    <a class="mobile_btn" id="mobile_btn">
+      <i class="fas fa-bars"></i>
+    </a>
+  </div>
+  <div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll"  >
+      <div id="sidebar-menu" class="sidebar-menu">
+        <ul>
+          <li>
+            <RouterLink :class="{ 'active': $route.name === 'home' }" to="/"><i class="feather-home"></i> <span>Home</span></RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.header-left {
+  width: 260px !important;
+}
+
+.remove-link-attr{
+  color: #455560;
+}
+
+@media only screen and (max-width: 991px) {
+  .wom-title {
+    margin-left: 20px;
+  }
+  .header-left {
+    width: 280px !important;
+  }
+}
+
+.sidebar-overlay {
+  background-color: unset;
+}
+</style>
+<script setup>
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-wrapper" id="main-wrapper">
+    <Header/>
+    <div class="page-wrapper">
+      <div class="content container-fluid">
+        <div class="page-header">
+          <div class="row">
+            <div class="col-sm-12">
+              <h3 class="page-title">Startseite</h3>
+              <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                <li class="breadcrumb-item active">Startseite</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: 'Home',
+  components: {Header},
+  async created() {
+  },
+}
+</script>
