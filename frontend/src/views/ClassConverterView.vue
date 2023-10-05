2fe3ac87e19c170cbe5c1986a955bd8683ac39d7
michael-mb <mbonimichael@yahoo.fr>
2023-10-05 12:48:36 +0200

    update

--- frontend/src/views/ClassConverterView.vue
+++ frontend/src/views/ClassConverterView.vue
@@ -81,9 +81,9 @@
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.aggregation)" > <i class="feather-file-plus me-2"></i> Aggregation</a>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.composition)" > <i class="feather-file-plus me-2"></i> Composition</a>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.simple_association)" > <i class="feather-file-plus me-2"></i> Simple Association</a>
+                    <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.association_class)" > <i class="feather-file-plus me-2"></i> Association Class</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.has_generalization_child)" > <i class="feather-file-plus me-2"></i> Has Gen_ Child</a>
-                    <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.association_class)" > <i class="feather-file-plus me-2"></i> Association Class</a>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.test_association)" > <i class="feather-file-plus me-2"></i> Has Association</a>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.count_methods)" > <i class="feather-file-plus me-2"></i> Count Methods</a>
                     <a class="dropdown-item" @click="addRule(rulesDefinitions.RULE_TYPE.count_attributes)"> <i class="feather-file-plus me-2"></i> Count Attributes</a>
