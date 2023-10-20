35a3f81f3cbf1c07339d4c30011979dc54442aa3
michael-mb <mbonimichael@yahoo.fr>
2023-10-20 08:40:03 +0200

    update imports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,3 @@
-// Template JS
-import './assets/js/jquery-3.6.0.min'
-import './assets/js/bootstrap.bundle.min.js'
-import './assets/js/feather.min.js'
-import './assets/plugins/slimscroll/jquery.slimscroll.min.js'
-import './assets/js/script.js'
-
-// Template CSS
-import './assets/main.css'
-import './assets/css/bootstrap.min.css'
-import './assets/plugins/fontawesome/css/fontawesome.min.css'
-import './assets/css/feather.css'
-import './assets/plugins/fontawesome/css/all.min.css'
-import './assets/css/style.css'
-
-
 import {createApp, defineAsyncComponent} from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
