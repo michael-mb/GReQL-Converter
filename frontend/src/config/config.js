a15431e80a00a660054d1f724513ef9a60505211
michael-mb <mbonimichael@yahoo.fr>
2023-10-25 15:17:36 +0200

    highlight version changed

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
-const BASE_API_URL = "http://localhost:3000"
-//const BASE_API_URL = "https://greql-converter-backend.vercel.app"
+//const BASE_API_URL = "http://localhost:3000"
+const BASE_API_URL = "https://greql-converter-backend.vercel.app"
 const API_ENDPOINTS = {
     CONVERT: "/convert"
 }
