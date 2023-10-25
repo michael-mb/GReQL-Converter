23c5d951121c63d700cd6eee78e73fb46cc05654
michael-mb <mbonimichael@yahoo.fr>
2023-10-25 21:15:52 +0200

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
   server: {
     proxy:{
         target: 'https://greql-converter-backend.vercel.app',
